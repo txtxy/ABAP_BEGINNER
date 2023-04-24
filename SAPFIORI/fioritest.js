7f6aade780fa689609b8a88baf4da7da7dcd28e4
MK_kang <gjhllkm@gmail.com>
2023-04-24 13:31:43 +0900

    fiori 1st

---
+++ SAPFIORI/fioritest.js
@@ -0,0 +1,33 @@
+// 한 줄 주석
+
+/*
+내용
+*/
+// console.log(1*3);
+// alert('Hi')
+
+
+// var name = 'moon';
+// if (true){
+//   var name = 'hong';
+  
+// }
+// console.log(name);
+
+let A ='Apple';
+let B = 123;
+let C = true;
+let D = [100,'hello',false];
+let F = {name : 'hong',
+        age : 20,
+        sleep : function(){
+          console.log('zzz...');
+        }
+        };
+
+console.log('A : ' + A);
+console.log('B : ' + B);
+console.log('C : ' + C);
+console.log('D : ' + D[1]);
+F.sleep();
+console.log(F['name']);
