c2dc13d537c3df382e278e6f2477435cbf8ec8e2
MK_kang <gjhllkm@gmail.com>
2023-04-25 19:32:12 +0900

    과제3완료

--- Class_note/FIORI/zui5_e01_01/zpeoject_e01_02/webapp/controller/Main.controller.js
+++ Class_note/FIORI/zui5_e01_01/zpeoject_e01_02/webapp/controller/Main.controller.js
@@ -1,5 +1,6 @@
 sap.ui.define([
-    "sap/ui/core/mvc/Controller"
+    "sap/ui/core/mvc/Controller",
+    "sap/m/Select"
 ],
     /**
      * @param {typeof sap.ui.core.mvc.Controller} Controller
@@ -9,7 +10,20 @@ sap.ui.define([
 
         return Controller.extend("zpeojecte0102.controller.Main", {
             onInit: function () {
+                debugger;
+                let input1 = this.byId("idInput1") ;
+                input1.setValue(10) ;
+                let input2 = this.byId("idInput2") ;
+                input2.setValue(20) ;
+                let select = this.byId("Select1");
+                select.setSelectedKey('multiply') 
+            },
+            onButtonPress: function (oEvent) {
+                // alert("오이벤트"+oEvent);
+                let sValue1 = this.byId("idInput1").getValue();
+                let sValue2 = this.byId("idInput2").getValue();
 
+                alert(sValue1 )
             }
         });
     });
