a8595f47c22d78568fa476557adc0d4ace1d0c80
MK_kang <gjhllkm@gmail.com>
2023-04-25 17:02:42 +0900

    Init_zproject_e01_02

---
+++ Class_note/FIORI/zui5_e01_01/zpeoject_e01_02/webapp/controller/App.controller.js
@@ -0,0 +1,14 @@
+sap.ui.define(
+    [
+        "sap/ui/core/mvc/Controller"
+    ],
+    function(BaseController) {
+      "use strict";
+  
+      return BaseController.extend("zpeojecte0102.controller.App", {
+        onInit() {
+        }
+      });
+    }
+  );
+  
